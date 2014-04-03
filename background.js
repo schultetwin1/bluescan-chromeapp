dcbe7cb9eea3b4e091e793052ccad3b037652555
Matt Schulte <schultetwin1@gmail.com>
2014-04-03 14:05:00 -0400

    [Blue Scan] Added Notifications
    We now have notifications which appear when we see a new device. Also, I rearranged the code make a lot more sense (like registering the listeners when the app is first created. I have no idea how it was working before). Also, added a simple site that you have to leave open while this is working

--- background.js
+++ background.js
@@ -1,41 +1,73 @@
 /* Track locaiton */
 var loc = null;
-chrome.location.watchLocation("", {});
-chrome.location.onLocationUpdate.addListener(function(position) {
-  console.log(JSON.stringify(position));
-  loc = position;
-});
-
-
-/* Resume BT scanning on power cycle */
 var powered = false;
-chrome.bluetooth.getAdapterState(function(adapter) {
-  powered = adapter.powered;
-});
-chrome.bluetooth.onAdapterStateChanged.addListener(
-  function(adapter) {
-    if (adapter.discovering == false) {
-      chrome.bluetooth.startDiscovery(function() {});
+var device_names = {};
+
+/* Open the page */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: 500,
+      height: 309
     }
-    if (adapter.powered != powered) {
-      powered = adapter.powered;
-      if (powered) {
-        console.log("Adapter radio is on");
-      } else {
-        console.log("Adapter radio is off");
+  });
+
+  chrome.location.watchLocation("", {});
+  chrome.location.onLocationUpdate.addListener(function(position) {
+    console.log(JSON.stringify(position));
+    loc = position;
+  });
+
+  /* Resume BT scanning on power cycle */
+  chrome.bluetooth.getAdapterState(function(adapter) {
+    powered = adapter.powered;
+  });
+  chrome.bluetooth.onAdapterStateChanged.addListener(
+    function(adapter) {
+      if (adapter.discovering == false) {
+        chrome.bluetooth.startDiscovery(function() {});
+      }
+      if (adapter.powered != powered) {
+        powered = adapter.powered;
+        if (powered) {
+          console.log("Adapter radio is on");
+        } else {
+          console.log("Adapter radio is off");
+        }
       }
     }
-  }
-);
+  );
+  // Add listeners to receive newly found devices and updates
+  // to the previously known devices.
+  chrome.bluetooth.onDeviceAdded.addListener(updateDeviceName);
+  chrome.bluetooth.onDeviceChanged.addListener(updateDeviceName);
+  chrome.bluetooth.onDeviceRemoved.addListener(removeDeviceName);
 
-var device_names = {};
+  // Now begin the discovery process.
+  chrome.bluetooth.startDiscovery(function() {});
+});
+
+chrome.runtime.onSuspend.addListener(function() {
+  chrome.bluetooth.stopDiscovery(function () {});
+});
+
 
 var updateDeviceName = function(device) {
   if (loc == null) return;
+
+  var opt = {
+    type: "basic",
+    title: "Scanned BLE Device",
+    message: "Found " + device.name,
+    iconUrl: "icon.png"
+  }
+  chrome.notifications.create("", opt, function(id){ console.log(id); });
+
   var xhr = new XMLHttpRequest();
   var data = new FormData();
+
   xhr.open("POST", "https://track-dev.schultetwins.com/api/v1.0/spot",true);
-  console.log("Device: " + device.name);
+
   data.append("MAC", device.address.toUpperCase().replace(/-/g, ":"));
   data.append("timestamp", (Math.floor(Date.now() / 1000)).toString());
   data.append("latitude", loc.coords.latitude.toString());
@@ -55,11 +87,3 @@ var removeDeviceName = function(device) {
   delete device_names[device.address];
 }
 
-// Add listeners to receive newly found devices and updates
-// to the previously known devices.
-chrome.bluetooth.onDeviceAdded.addListener(updateDeviceName);
-chrome.bluetooth.onDeviceChanged.addListener(updateDeviceName);
-chrome.bluetooth.onDeviceRemoved.addListener(removeDeviceName);
-
-// Now begin the discovery process.
-chrome.bluetooth.startDiscovery(function() {});
